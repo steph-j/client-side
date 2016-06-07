e3de4a29cc5e32f200ef70237d34ed663302ef71
EDA <eda@mac-mini-1.local>
2016-06-07 15:22:04 +1200

    Updates

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ var example = require('./views/example.hbs')
 
 xhr.get('https://api.wheretheiss.at/v1/satellites', function (err, data) {
   if (err) {
-    console.log(err)
+    console.log(err + "Errrr ... try again")
   }
 
   console.log(data.body) // in case you're curious
+  console.log(data.body[0]) // in case you're curious
+  console.log(data.body[0].name) // in case you're curious
+  console.log(data.body.name);
 
-  document.body.innerHTML = example({
-    name: 'Space'
-  })
+
+  document.body.innerHTML = example(data.body)
 })
